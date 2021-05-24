a2a053d63392dcac019cbfa6441e0b44c3d032e3
Hendrik Bulens <hendrik.bulens@gmail.com>
2021-05-24 09:59:03 +0200

    Container import models

---
+++ src/core/Import/Container.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dime.Scheduler.Sdk.Import
+{
+
+    public class Container : IImportRequestable, IValidatableImportRequest<Container>
+    {
+        public Container()
+        {
+
+        }
+        
+        [Required]
+        [ImportParameter(nameof(Name))]
+        public string Name { get; set; }
+
+        ImportRequest IImportRequestable.ToImportRequest(TransactionType transactionType)
+            => transactionType switch
+            {
+                TransactionType.Append => ((IValidatableImportRequest<Container>)this).Validate(transactionType).CreateAppendRequest(),
+                TransactionType.Delete => ((IValidatableImportRequest<Container>)this).Validate(transactionType).CreateDeleteRequest(),
+                _ => throw new ArgumentOutOfRangeException(nameof(transactionType), transactionType, null)
+            };
+
+        private ImportRequest CreateAppendRequest()
+            => new(ImportProcedures.Container.Append, this.CreateParameters(TransactionType.Append));
+
+        private ImportRequest CreateDeleteRequest()
+            => new(ImportProcedures.Container.Delete, this.CreateParameters(TransactionType.Delete));
+
+        Container IValidatableImportRequest<Container>.Validate(TransactionType transactionType)
+            => this.Validate(transactionType);
+
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+            => this.Validate<Container>(validationContext);
+    }
+}
