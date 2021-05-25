8e9dca88f28d9d15b052ba162070626cec57b210
Hendrik Bulens <hendrik.bulens@dimenics.com>
2021-05-25 11:55:14 +0200

    Containers (#16)
    * Container import models
    * Container properties
    * Container property attribute config

---
+++ src/core/Import/AppointmentContainer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dime.Scheduler.Sdk.Import
+{
+    public class AppointmentContainer : IImportRequestable, IValidatableImportRequest<AppointmentContainer>
+    {
+        [Required]
+        [ImportParameter(nameof(Container))]
+        public string Container { get; set; }
+
+        [Required]
+        [ImportParameter(nameof(Appointment))]
+        public string Appointment { get; set; }
+
+        ImportRequest IImportRequestable.ToImportRequest(TransactionType transactionType)
+            => transactionType switch
+            {
+                TransactionType.Append => ((IValidatableImportRequest<AppointmentContainer>)this).Validate(transactionType).CreateAppendRequest(),
+                TransactionType.Delete => ((IValidatableImportRequest<AppointmentContainer>)this).Validate(transactionType).CreateDeleteRequest(),
+                _ => throw new ArgumentOutOfRangeException(nameof(transactionType), transactionType, null)
+            };
+
+        private ImportRequest CreateAppendRequest()
+            => new(ImportProcedures.AppointmentContainer.Append, this.CreateParameters(TransactionType.Append));
+
+        private ImportRequest CreateDeleteRequest()
+            => new(ImportProcedures.AppointmentContainer.Delete, this.CreateParameters(TransactionType.Delete));
+
+        AppointmentContainer IValidatableImportRequest<AppointmentContainer>.Validate(TransactionType transactionType)
+            => this.Validate(transactionType);
+
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+            => this.Validate<AppointmentContainer>(validationContext);
+    }
+}
---
+++ src/test/Import/AppointmentContainerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Dime.Scheduler.Sdk.Import;
+using Xunit;
+
+namespace Dime.Scheduler.Sdk.Tests.Import
+{
+    public class AppointmentContainerTests
+    {
+        [Fact]
+        public void AppointmentContainer_ToImportRequest_Append_AllShouldMapParameters()
+        {
+            AppointmentContainer model = new()
+            {
+                Container = "Container 1",
+                Appointment = "Appointment 1"
+            };
+
+            ImportRequest importRequest = model.ToImportRequest(TransactionType.Append);
+            importRequest.AssertEqualParameterCollectionCount();
+        }
+
+        [Fact]
+        public void AppointmentContainer_Append_Validate_HasRequiredAttributes_ShouldSucceed()
+        {
+            AppointmentContainer model = new() { Container = "Container 1", Appointment = "Appointment 1" };
+            ImportRequest importRequest = ((IImportRequestable)model).ToImportRequest(TransactionType.Append);
+
+            Assert.True(importRequest.ParameterNames[0] == "Container");
+            Assert.True(importRequest.ParameterValues[0] == "Container 1");
+        }
+
+        [Fact]
+        public void AppointmentContainer_Append_Validate_MissingRequiredAttributes_ShouldThrowException()
+        {
+            AppointmentContainer model = new();
+            Assert.Throws<Exception>(() => ((IImportRequestable)model).ToImportRequest(TransactionType.Append));
+        }
+
+        [Fact]
+        public void AppointmentContainer_ToImportRequest_Delete_AllShouldMapParameters()
+        {
+            AppointmentContainer model = new()
+            {
+                Container = "Container 1",
+                Appointment = "Appointment 1"
+            };
+
+            ImportRequest importRequest = model.ToImportRequest(TransactionType.Delete);
+            importRequest.AssertEqualParameterCollectionCount();
+        }
+
+        [Fact]
+        public void AppointmentContainer_Delete_Validate_MissingRequiredAttributes_ShouldThrowException()
+        {
+            AppointmentContainer model = new();
+            Assert.Throws<Exception>(() => ((IImportRequestable)model).ToImportRequest(TransactionType.Delete));
+        }
+    }
+}
