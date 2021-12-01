bfdc7f97feab096add60cb9c2cdca1a7f6e70af5
Hendrik Bulens <hendrik.bulens@gmail.com>
2021-12-01 22:06:29 +0100

    Activate license #72

--- src/core/DimeSchedulerClient.cs
+++ src/core/DimeSchedulerClient.cs
@@ -9,11 +9,13 @@
             Messages = new MessagesEndpointBuilder(baseUrl, authenticator);
             Users = new UsersEndpointBuilder(baseUrl, authenticator);
             Resources = new ResourcesEndpointBuilder(baseUrl, authenticator);
+            License = new LicensesEndpointBuilder(baseUrl, authenticator);
         }
 
         public IImportEndpointBuilder Import { get; }
         public IMessageEndpointBuilder Messages { get; }
         public IUserEndpointBuilder Users { get; }
         public IResourceEndpointBuilder Resources { get; }
+        public ILicenseEndpointBuilder License { get; }
     }
 }
---
+++ src/test/Endpoints/LicenseEndpointTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Dime.Scheduler.Sdk.Tests
+{
+    public class LicenseEndpointTests
+    {
+        [Fact]
+        public async Task LicenseEndpoint_MockClient_Create_ShouldCreate()
+        {
+            LicenseEndpoint endpoint = new(new MockRestClient<LicenseRequest>());
+            await endpoint.PostAsync(new LicenseRequest());
+        }
+    }
+}
