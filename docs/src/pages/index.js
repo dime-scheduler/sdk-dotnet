c55c90422c3e2f4d9b69b58576bc4e7253e8be07
Hendrik Bulens <hendrik.bulens@gmail.com>
2022-03-09 09:29:05 +0100

    Bump docs packages

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -109,7 +109,7 @@ function Home() {
       description="Dime.Scheduler Software Development Kit">
       <header className={clsx('hero hero--primary heroTilted_3die', styles.heroBanner)}>
         <div className="container">
-          <img src="img/ds.svg" height="100" alt="Dime.Scheduler logo" />
+          <img src="img/ds.svg" width="500" alt="Dime.Scheduler logo" />
           <p className="hero__subtitle">Software Development Kit</p>
           <div className={styles.buttons}>
             <Link
