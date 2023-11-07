d22aae400f4870e609e3a1905c45c562d3618617
Hendrik Bulens <hendrik.bulens@gmail.com>
2023-11-07 09:09:41 +0100

    Undo notification type

--- src/core/Constants/NotificationType.cs
+++ src/core/Constants/NotificationType.cs
@@ -2,11 +2,8 @@
 {
     public enum NotificationType
     {
-        Verbose = 0,
-        Debug = 1,
-        Info = 2,
-        Warning = 4,
-        Error = 8,
-        Fatal = 18
+        Info = 0,
+        Warning = 1,
+        Error = 2
     }
 }
