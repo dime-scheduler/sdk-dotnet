433990fc213ab7d8c8e4610f530f980cb17865bd
Hendrik Bulens <hendrik.bulens@gmail.com>
2023-11-29 12:31:25 +0100

    Example console app

---
+++ examples/console/AppendTask.cs
@@ -0,0 +1,41 @@
+﻿using Dime.Scheduler;
+using Spectre.Console;
+
+public class AppendTask
+{
+    public AppendTask(Dime.Scheduler.Environment environment, string key)
+    {
+        Environment = environment;
+        Key = key;
+    }
+
+    private Dime.Scheduler.Environment Environment { get; }
+    private string Key { get; }
+
+    public async Task Run()
+    {
+        DimeSchedulerClient client = new(Key, Environment);
+
+        string name = AnsiConsole.Ask<string>("Name of this task:");
+        string descr = AnsiConsole.Ask<string>("Short description:");
+
+        // Required fields in a task include Source App, Source Type, Job No, Task No, Short Description
+        // By setting the CreateJob property to true, Dime.Scheduler will automatically create a job for this task
+        Dime.Scheduler.Entities.Task task = new()
+        {
+            SourceApp = "EXAMPLE",
+            SourceType = "EXAMPLE",
+            JobNo = name,
+            TaskNo = name,
+            Description = descr,
+            ShortDescription = descr.Length < 50 ? descr : descr.Substring(0, 50),
+            CreateJob = true
+        };
+
+        await AnsiConsole.Status().Start("Saving...", async ctx =>
+        {
+            await client.Tasks.CreateAsync(task);
+            Thread.Sleep(2000);
+        });
+    }
+}
---
+++ examples/console/Program.cs
@@ -0,0 +1,42 @@
+﻿using Spectre.Console;
+
+bool again = true;
+do
+{
+    // Step 1: action selection
+    string action = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+            .Title("What would you like to try out?")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to show more examples)[/]")
+            .AddChoices(new[] { "Task", "Resource" }));
+
+    // Step 2: environment selection
+    string environment = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("What environment should we be running the command on?")
+        .PageSize(10)
+        .AddChoices(new[] { "Production", "Sandbox" }));
+
+    Dime.Scheduler.Environment env = environment == "Production" ? Dime.Scheduler.Environment.Production : Dime.Scheduler.Environment.Sandbox;
+
+    // Step 3: API key
+    string key = AnsiConsole.Ask<string>("Please share the API key to use:");
+
+    switch (action)
+    {
+        case "Task":
+            AppendTask taskCmd = new(env, key);
+            await taskCmd.Run();
+            break;
+
+        case "Resource":
+            AppendResource resourceCmd = new(env, key);
+            await resourceCmd.Run();
+            break;
+    }
+
+    if (!AnsiConsole.Confirm("Want to do anything else?"))
+        again = false;
+}
+while (again);
